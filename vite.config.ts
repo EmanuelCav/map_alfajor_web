0ea52a42046c81633fe6fad113bc724eb6988dd8
EmanuelCav <cavallinema@gmail.com>
2025-01-30 12:05:34 -0300

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false
+  },
   plugins: [react(), tailwindcss()],
 })
