00e4d6646db01da7854b3d893a4411c075ce1abd
EmanuelCav <cavallinema@gmail.com>
2025-01-30 17:03:48 -0300

    Loading resize and add meta robots

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -2,7 +2,7 @@
 const Loading = () => {
     return (
         <div className="h-screen w-full flex items-center justify-center flex-col">
-            <img src="/loading.gif" alt="loading..." className="w-28 h-28" />
+            <img src="/loading.gif" alt="loading..." className="w-20 h-20" />
             <p className="text-amber-600 mt-4 text-xl font-semibold">Cargando</p>
         </div>
     )
